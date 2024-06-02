dc1dceb240f04e3ff06e08a5f20d6112dccdc72c
frm192820 <firshol729@gmail.com>
2024-06-02 16:06:56 +0700

    settings.js

--- settings.js
+++ settings.js
@@ -75,7 +75,7 @@ global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.tag_owner = "./tag_owner.mp3" // audio ini akan di kirim apabila ada member yang menandai (tag) owner bot 
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.thumbGif = 'https://telegra.ph/file/e90bef3779d3c5812a5d1.mp4'
-global.packname = '© ' + Man //sticker wm ubah
+global.packname = '© ' + botname //sticker wm ubah
 global.author = 'man By ' + ownername //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.push_kontak_delay = 5000 //5 detik, 1000 = 1 detik
